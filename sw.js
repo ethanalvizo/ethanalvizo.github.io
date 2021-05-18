eebb945eaafbb93f89dde5864c0b6038df89d167
ethanalvizo <55671206+ethanalvizo@users.noreply.github.com>
2021-05-18 17:50:52 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2264dcfe17d2364d567f.js"
+    "url": "webpack-runtime-098b2bf21b885dfb8fa9.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b207e88c7152af55eb35fc3de45450be"
+    "revision": "d315214911bb479808d30624de66de2e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "06832df2bed499cd566ea5aefd4b456e"
+    "revision": "631b1390f8d4fa9201b81c3bf1872392"
   },
   {
     "url": "polyfill-57fa76c8ad53bd44c5a1.js"
