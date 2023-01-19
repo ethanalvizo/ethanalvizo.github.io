0e06e5fed3bc48f9645c5eb31fb82c450a3048d8
Ethan Alvizo <ealvizo@tangamgaming.com>
2023-01-19 17:26:53 -0500

    Fixed empty images in deployed site

--- src/components/data/portfolio.js
+++ src/components/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
   {
     title: "VR Playground",
-    imageUrl: "/public/assets/vr-playground.png",
+    imageUrl: "/assets/vr-playground.png",
     stack: ["Unity", "C#", "Oculus Quest"],
     link: "https://drive.google.com/drive/folders/17xNgoc8eFK-FIPkOdYJwW3ZPIj3d3rhi",
   },
   {
     title: "Grocery Assistant",
-    imageUrl: "/public/assets/grocery-assistant.png",
+    imageUrl: "/assets/grocery-assistant.png",
     stack: ["React Native", "AWS"],
     link: "https://github.com/ethanalvizo/recipe-remy",
   },
   {
     title: "Hand Gesture Detector",
-    imageUrl: "/public/assets/hand-gesture-detector.png",
+    imageUrl: "/assets/hand-gesture-detector.png",
     stack: ["Python", "Arduino", "Ultrasound"],
     link: "https://docs.google.com/presentation/d/1yOvbln0euZJmIH8IPSigYqhlVgEmFGSsmv1nIyOtnJA/edit?usp=sharing",
   },
