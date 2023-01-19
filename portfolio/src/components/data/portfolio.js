f00d96fe0e7cb4d2b2ac57f383452558625d1731
Ethan Alvizo <ethanalvizo@gmail.com>
2023-01-19 01:44:34 -0500

    Updates images, descriptions, and links

--- portfolio/src/components/data/portfolio.js
+++ portfolio/src/components/data/portfolio.js
@@ -3,18 +3,18 @@ export default [
     title: "VR Playground",
     imageUrl: "/public/assets/vr-playground.png",
     stack: ["Unity", "C#", "Oculus Quest"],
-    link: "https://github.com",
+    link: "https://drive.google.com/drive/folders/17xNgoc8eFK-FIPkOdYJwW3ZPIj3d3rhi",
   },
   {
     title: "Grocery Assistant",
-    imageUrl: "/public/assets/grocery.png",
+    imageUrl: "/public/assets/grocery-assistant.png",
     stack: ["React Native", "AWS"],
-    link: "https://github.com",
+    link: "https://github.com/ethanalvizo/recipe-remy",
   },
   {
     title: "Hand Gesture Detector",
-    imageUrl: "/public/assets/car.png",
+    imageUrl: "/public/assets/hand-gesture-detector.png",
     stack: ["Python", "Arduino", "Ultrasound"],
-    link: "https://github.com",
+    link: "https://docs.google.com/presentation/d/1yOvbln0euZJmIH8IPSigYqhlVgEmFGSsmv1nIyOtnJA/edit#slide=id.p",
   },
 ];
--- portfolio/src/components/data/timeline.js
+++ portfolio/src/components/data/timeline.js
@@ -3,32 +3,33 @@ export default [
     year: "Today",
     title: "Software Developer",
     company: "Tangam Systems",
-    stack: ["TypeScript", "Angular", "RxJS", "C#/ASP.NET", "SQL"],
+    stack: ["TypeScript", "Angular", "C#", "ASP.NET", "SQL"],
     details:
       "Building data visualization software for casinos to optimize profits and player experience. Improving upon user experience and application visibility of a 10+ year old product",
+  },
+  {
+    year: "2022",
+    title: "Project Developer",
+    company: "UW Blueprint",
+    stack: ["REST API", "TypeScript", "Express", "Node", "Docker"],
+    details:
+      "Developed REST API endpoints for a national non-profit organization's book reviewing platform. Implemented Figma mockups from UI/UX team, ensuring 100% responsiveness",
   },
   {
     year: "2022",
     title: "Front-End & Game Developer",
     company: "AI Arena",
     stack: ["React", "D3", "ThreeJS", "WebGL", "GLSL"],
-    details:
-      "Architected responsive UI for central features such as NFT marketplace, leaderboard, and locker room in a Web3 game to train AI fighters. Created landing pages utilizing both 2D and 3D components to boost engagement during marketing efforts. Implemented stage backgrounds and loading animations from the design team",
-  },
-  {
-    year: "2022",
-    title: "Project Developer",
-    company: "UW Blueprint - Canadian Children's Book Centre Team",
-    stack: ["REST API", "TypeScript", "Express", "Node", "Docker"],
-    details:
-      "Developed REST API endpoints for a national non-profit organization's book reviewing platform. Implemented Figma mockups from UI/UX team, ensuring 100% responsiveness",
+    details: "Combined 2D and 3D components for core features such as the NFT marketplace, leaderboard, and locker room in a Web3 game. Implemented in-game animations for immersion and marketing"
+    // details:
+      // "Architected responsive UI for central features such as NFT marketplace, leaderboard, and locker room in a Web3 game to train AI fighters. Created landing pages utilizing both 2D and 3D components to boost engagement during marketing efforts. Implemented stage backgrounds and loading animations from the design team",
   },
   {
     year: "2021",
     title: "Front-End Engineer",
     company: "VTS",
     stack: ["React Native", "TypeScript", "GraphQL", "iOS/Android", "Jest"],
-    details: "",
+    details: "Created the primary user flow for room booking and building management for commercial real estate. Implemented reusable and responsive components for iOS and Android platform",
   },
   {
     year: "2021",
@@ -36,13 +37,13 @@ export default [
     company: "Betterworth",
     stack: ["MongoDB", "Express", "React", "Node", "Material-UI"],
     details:
-      "Built MVP to aid financial advisor relations with clients using the MERN stack and MVC pattern. Automated the revision of platform content using the Google Cloud Platform, accelerating the release cycle by 25x",
+      "Revamped UI and built friends/invite system for a dashboard to improve financial advisor-client relations. Accelerated release cycle by automating revision of platform copywrite",
   },
   {
     year: "2020",
     title: "Software Developer",
     company: "IntelliCulture",
-    stack: ["HTML5", "CSS3", "JavaScript", "Git"],
+    stack: ["HTML", "CSS", "JavaScript", "Bootstrap", "Git"],
     details:
       "Prototyped equipment maintenance dashboard that collected vehicle information and automatically scheduled service appointments, reducing farmer's repair costs by $10,000/year",
   },
